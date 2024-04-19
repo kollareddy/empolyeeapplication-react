d284b4303afa3233d73e1c99131d09efc22364dc
kollareddy <159079488+kollareddy@users.noreply.github.com>
2024-04-18 21:13:47 -0400

    status

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Title from './components/title.jsx'
-import data from './data/data.js'
+import data from '../data/data.js'
 import Container from '@mui/material/Container';
 import './App.css'
+
 import MediaCard from './components/employeecard.jsx'
+
 function App() {
   const [count, setCount] = useState(0)
 console.log(data[0].firstName);
   return (
     <Container fixed>
-   <MediaCard />
+   <MediaCard employee={data} />
    </Container>
    
   )
