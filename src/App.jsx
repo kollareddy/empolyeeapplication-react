36bc771639a784e336d486e5bb88ef7f2b9ef2a0
kollareddy <159079488+kollareddy@users.noreply.github.com>
2024-04-25 21:01:31 -0400

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,45 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Title from './components/title.jsx'
-import data from '../data/data.js'
+import Title from './components/title'
+import MediaCard from './components/employeecard.jsx';
 import Container from '@mui/material/Container';
-import './App.css'
+import Switch from '@mui/material/Switch';
+import Typography from '@mui/material/Typography';
 
-import MediaCard from './components/employeecard.jsx'
+
+
+
+
+console.log(data[0].firstName);
+
+import data from './data/data.js';
+import './App.css'
+const label = { inputProps: { 'aria-label': 'Switch demo' } };
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-console.log(data[0].firstName);
+  const[ShowInactive,setShowInactive] = useState(false);
+
+const filterData = ShowInactive ? data.filter((employee) => employee.onLeave) :data;
+
+const handleToggle = () => {
+  setShowInactive(event.target.checked);
+  console.log(event.target.checked);
+ }
+
+  console.log(ShowInactive);
+  
   return (
     <Container fixed>
-   <MediaCard employee={data} />
-   </Container>
-   
+      <Typography variant="h3" component="h2">
+  Employee Dashboard
+</Typography>
+
+      <Switch {...label} checked={ShowInactive}onClick={handleToggle}/>
+    <MediaCard employee={filterData} />
+    </Container>
   )
 }
 
--- src/components/employeecard.jsx
+++ src/components/employeecard.jsx
@@ -5,41 +5,52 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import AirplanemodeActiveIcon from '@mui/icons-material/AirplanemodeActive';
+import PhotoSizeSelectActualOutlinedIcon from '@mui/icons-material/PhotoSizeSelectActualOutlined';
 import Chip from '@mui/material/Chip';
 import Stack from '@mui/material/Stack';
 
-export default function MediaCard({employee})  {
+export default function MediaCard({ employee }) {
   console.log(employee);
   return (
-    <Card sx={{ maxWidth: 345, margin: 10, padding: 1 }}>
-      <CardMedia
-        sx={{ height: 200, backgroundSize: 'contain', margin: 1}}
-        image={employee[0].image}
-        title="profile"
-      />
-      <CardContent>
-        <AirplanemodeActiveIcon />
+    <>
+      {employee.map((item, index) => (
+        <Card key={index} sx={{ maxWidth: 345, margin: 10, padding: 1 }}>
+          <CardMedia
+            sx={{ height: 200, backgroundSize: "contain", margin: 1 }}
+            image={`/ReactEmployeeDetails/${item.image}`}
+            title="profile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {item.firstName} {item.lastName}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {item.department}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {item.position}
+            </Typography>
+          </CardContent>
 
-        <Typography gutterBottom variant="h5" component="div">
-          {employee[0].firstName} {employee[0].lastName}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {employee[0].deparment}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {employee[0].position}
-        </Typography>
+          <Stack> 
+          {item.skills.map((skill, skillIndex) => (
+<Chip 
+variant="outlined"
+sx={{ marginTop: "10px"}}
+key= {skillIndex}
+label= {skill}
+ />
+          )
+        
+        )}
+          </Stack>
 
-      </CardContent>
-      <Stack direction="row" spacing={1}>
-      <Chip label="Chip Filled" />
-      <Chip label="Chip Outlined" variant="outlined" />
-    </Stack>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
-  );
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+      ))}
+  </>
+ );
 }
